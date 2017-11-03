4c8f7352b3a2f3761a9f6f510f7e249371a40e67
“puribey” <kenshipichu@gmail.com>
2017-11-03 18:01:26 -0300

    DOM for dummies

---
+++ form/script.js
@@ -0,0 +1,31 @@
+
+
+function validacion(){
+	var n= document.getElementById("nombre");
+	var p= document.getElementById("password");
+	if (n.value == "" && p.value == 0){ // el "" y el 0 son lo mismo 
+		alert(`Usuario y contraseña vacias!`);
+	}
+}
+
+
+
+
+
+
+/* 
+
+
+function changeColor(color){
+	var e=document.getElementById('test');
+	e.style.color=color;
+}
+
+
+
+
+function newDoc() {
+    window.location.assign("https://www.w3schools.com")
+	}	
+
+*/
