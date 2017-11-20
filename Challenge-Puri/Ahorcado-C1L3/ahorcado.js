b2192dc91131e841d7c49fd1c75e0c8bd9a504b6
“puribey” <kenshipichu@gmail.com>
2017-11-20 19:15:31 -0300

    ahorcado

--- Challenge-Puri/Ahorcado-C1L3/ahorcado.js
+++ Challenge-Puri/Ahorcado-C1L3/ahorcado.js
@@ -17,8 +17,49 @@ var restaPuntos = 5;
 var nivel1 = [];
 var palMagica = document.getElementById('palMagica');
 var divpalabra = document.getElementById('divpalabra');
-
+var letra;
+var x = [];
 //console.log(treinta[0].length);
+
+
+/*********** BOTONES CON LETRAS ************/
+var html = '';
+var c;
+for (var i = 65; 90 >= i; i++) {// A-65, Z-90
+  c = String.fromCharCode(i);
+  // console.log(c); //para ver las letras 
+  html += '<button onclick="setLetter(\'' + c + '\');" id="'+ c +'" class="btnLetra">' + c + '</button>';
+}
+document.getElementById('box').innerHTML = html;
+
+var setLetter = function(alfabeto) {
+
+  //divpalabra.innerHTML += alfabeto;
+  puntosGame = puntosGame - restaPuntos;
+  document.getElementById('divPuntos').innerHTML = '<p class="txt">El total de puntos del nivel es de ' + puntosGame + ' puntos </p>';
+  //console.log(alfabeto);
+
+  for (var j=0; j<nivel1.length;j++){
+  	var cont = nivel1[j].length; //cantidad de letras que tiene cada palabra
+  	//console.log(cont); 
+  	if (nivel1[j] == nivel1[0]){
+  		for (var k=0 ; k<cont; k++){
+  			//console.log(nivel1[j][k]); // cada letra de cada palabra 
+  			if (nivel1[j][k]== alfabeto) {
+  				//console.log(nivel1[j][k]);
+  				x[k] = alfabeto;
+  				divpalabra.innerHTML += alfabeto + '(' +k + ')';	
+  				console.log(x[k]);
+  				
+  			} 
+  		}
+  	}
+};
+
+
+
+
+/************* SELECCION DE PALABRAS ************/
 
 for (var i =0; i<treinta.length;i++){
 		p = treinta[i].length;
@@ -34,35 +75,37 @@ for (var i =0; i<treinta.length;i++){
 // console.log(bajo); console.log(medio); console.log(alto);
 
 
-function nivelUno(){
-	for (var i=0; i<=3;i++){
-		var elegido = Math.floor(Math.random()*bajo.length);
-		//console.log(bajo[elegido]);
-		nivel1.push(bajo[elegido]);
-		bajo.splice(elegido,1);
-		//console.log(` este es el array del nivel 1: ${nivel1}`);
-		if(nivel1[i].length == 5){
-			puntosGame = puntosGame + 25;
-		} else {
-			puntosGame = puntosGame + 30;
-		}
+for (var i=0; i<=3;i++){
+	var elegido = Math.floor(Math.random()*bajo.length);
+	//console.log(bajo[elegido]);
+	nivel1.push(bajo[elegido]);
+	bajo.splice(elegido,1);
+	//console.log(` este es el array del nivel 1: ${nivel1}`);
+	
+	if(nivel1[i].length == 5){
+		puntosGame = puntosGame + 25;
+	} else {
+		puntosGame = puntosGame + 30;
 	}
+
+}	
+
 	document.getElementById('divPuntos').innerHTML = '<p class="txt">El total de puntos del nivel es de ' + puntosGame + ' puntos </p>';
 	//console.log(puntosGame);
 	// return nivel1;
+
 	for (var j=0; j<nivel1.length;j++){
 		var cont = nivel1[j].length; //cantidad de letras que tiene cada palabra
 		console.log(cont); 
-
-		for (var k=0 ; k<cont ; k++){
-			console.log(nivel1[j][k]); // cada letra de cada palabra 
-			palMagica.textContent += "X";
+		if (nivel1[j] == nivel1[0]){
+			for (var k=0 ; k<cont; k++){
+				console.log(nivel1[j][k]); // cada letra de cada palabra 
+				x.push("X");
+				palMagica.textContent = x;	
+			}
 		}
 	}
 	
-}
-
-nivelUno();
 
 
 
@@ -71,25 +114,7 @@ nivelUno();
 
 
 
-var html = '';
-var c;
-for (var i = 65; 90 >= i; i++) {// A-65, Z-90
-  c = String.fromCharCode(i);
-  // console.log(c); //para ver las letras 
-  html += '<button onclick="setLetter(\'' + c + '\');" id="'+ c +'" class="btnLetra">' + c + '</button>';
-}
-document.getElementById('box').innerHTML = html;
 
-
-
-var setLetter = function(alfabeto) {
-
-  divpalabra.innerHTML += alfabeto;
-  puntosGame = puntosGame - restaPuntos;
-  document.getElementById('divPuntos').innerHTML = '<p class="txt">El total de puntos del nivel es de ' + puntosGame + ' puntos </p>';
-
-  
-};
 
 
 
